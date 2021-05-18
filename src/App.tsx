5b21176fd2f94d0d8d20a5decc66765d46b54683
Majchrzak <majchrzakmail@gmail.com>
2021-05-18 20:50:03 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,9 @@ function App() {
   return (
     <Container>
       <Row>
+        <Col>{profile.overview.name}</Col>
+      </Row>
+      <Row>
         <Col md="12" lg="6">
           <Section title="About me">
             <p className="py-1">{profile.about}</p>
@@ -41,6 +44,22 @@ function App() {
               ))}
             </ul>
           </Section>
+          <Section title="Certification">
+            <ul>
+              {profile.certification.map(it => (
+                <li>{it}</li>
+              ))}
+            </ul>
+          </Section>
+          <div>
+            I agree to the processing of personal data provided in this document for
+            realising the recruitment process pursuant to the Personal Data Protection Act
+            of 10 May 2018 (Journal of Laws 2018, item 1000) and in agreement with
+            Regulation (EU) 2016/679 of the European Parliament and of the Council of 27
+            April 2016 on the protection of natural persons with regard to the processing
+            of personal data and on the free movement of such data, and repealing
+            Directive 95/46/EC (General Data Protection Regulation)
+          </div>
         </Col>
         <Col>
           <Section title="Commercial experience">
